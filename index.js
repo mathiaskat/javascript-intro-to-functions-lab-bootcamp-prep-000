0804dcccdff766dd7eea196dfe3a03fbdcc74020
KYAMBADDE MATHIAS KATUMBA <kyamathias@yahoo.co.uk>
2019-01-05 16:01:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-
+var uppercase, lowercase
+ 
+function shout(string){
+  uppercase = string.toUpperCase();
+  string = uppercase;
+  return uppercase;
+}
+function whisper(string){
+  lowercase = string.toLowerCase();
+  string = lowercase;
+  return lowercase;
+}
+function logShout(string){
+  uppercase = string.toUpperCase()
+  string = uppercase;
+  console.log(uppercase);
+}
+function logWhisper(string){
+  lowercase = string.toLowerCase()
+  string = uppercase;
+  console.log(lowercase);
+}
 function sayHiToGrandma(string){
     if (string.toLowerCase() === string){
       return 'I can\'t hear you!';
